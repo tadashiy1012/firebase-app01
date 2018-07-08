0afff4132eb06f11f1027d6af1f5cfd55dee437c
TadashiYamazaki <yama3ki.tada4@gmail.com>
2018-07-08 13:16:49 +0900

    ログイン機能のあれこれ

--- public/script.js
+++ public/script.js
@@ -1,28 +1,72 @@
 {
-  console.log("ready!");
-  document.addEventListener('DOMContentLoaded', function() {
-    try {
-      const app = firebase.app();
-      const provider = new firebase.auth.GoogleAuthProvider();
-      firebase.auth().getRedirectResult().then(function(result) {
-        let token = null;
+  const config = {
+    apiKey: "AIzaSyCGyH8E4JZRdlFzpaV2ArouDGP6AMfItFk",
+    authDomain: "test-pj01-72bea.firebaseapp.com",
+    databaseURL: "https://test-pj01-72bea.firebaseio.com",
+    projectId: "test-pj01-72bea",
+    storageBucket: "test-pj01-72bea.appspot.com",
+    messagingSenderId: "478620108057"
+  };
+  function login() {
+    return new Promise((resolve, reject) => {
+      Promise.resolve().then(() => {
+        return firebase.auth().setPersistence(firebase.auth.Auth.Persistence.SESSION);
+      }).then(() => {
+        return firebase.auth().getRedirectResult()
+      }).then((result) => {
         if (result.credential) {
-          token = result.credential.accessToken;
+          console.log(result.credential.accessToken);
         }
         let user = result.user;
         if (user === null || user === void 0) {
-          firebase.auth().signInWithRedirect(provider);
+          resolve([false, user]);
         } else {
-          console.log("login success!");
-          console.log(user, token);
-          document.getElementById("loginResult").innerText = "login success!";
+          resolve([true, user]);
         }
-      }).catch(function(err) {
+      }).catch((err) => {
         console.error(err);
-        firebase.auth().signInWithRedirect(provider);
-      })
-    } catch (e) {
-      console.error(e);
-    }
-  });
+        reject(err);
+      });
+    });
+  }
+  function authCheck() {
+    return new Promise((resolve, reject) => {
+      firebase.auth().onAuthStateChanged((result) => {
+        if (result) {
+          resolve([true, result]);
+        } else {
+          resolve([false, result]);
+        }
+      });
+    });
+  }
+  function init() {
+    firebase.initializeApp(config);
+    firebase.app();
+    Promise.resolve().then(() => {
+      return authCheck();
+    }).then((result) => {
+      console.log(result);
+      if (!result[0]) {
+        login().then((result) => {
+          console.log(result);
+          if (result[0]) {
+            console.log(result[1]);
+            document.getElementById("loginResult").innerText = "login ok!";
+          } else {
+            document.getElementById("loginResult").innerText = "login ng!";
+            const provider = new firebase.auth.GoogleAuthProvider();
+            firebase.auth().signInWithRedirect(provider);
+          }
+        }).catch((err) => {
+          console.error(err);
+        });
+      } else {
+        document.getElementById("loginResult").innerText = "login ok!";
+      }
+    });
+    
+  }
+  
+  init();
 }
